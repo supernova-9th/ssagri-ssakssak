c3085292a8689b5fe03114acb8fac8bbf6143a4b
dorianharok <dorianharok@github.com>
2024-08-22 19:52:43 +0900

    feat: UserDetails 구현

---
+++ src/main/java/com/supernova/ssagrissakssak/core/security/LoginUser.java
@@ -0,0 +1,57 @@
+package com.supernova.ssagrissakssak.core.security;
+
+import com.supernova.ssagrissakssak.feed.persistence.repository.entity.UserEntity;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class LoginUser implements UserDetails {
+
+    private final UserEntity user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+}
