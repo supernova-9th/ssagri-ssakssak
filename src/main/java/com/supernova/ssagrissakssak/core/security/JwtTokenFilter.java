23f6c2322a7af61842e71dadbaa390da8e4c6df2
dorianharok <dorianharok@github.com>
2024-08-22 20:31:51 +0900

    feat: token filter 구현

---
+++ src/main/java/com/supernova/ssagrissakssak/core/security/JwtTokenFilter.java
@@ -0,0 +1,44 @@
+package com.supernova.ssagrissakssak.core.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final UserDetailsService userDetailsService;
+
+    private static final String API_WHITE_LIST = "/auth/.*";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = jwtProvider.extractToken(request);
+
+        if(token != null && jwtProvider.validateToken(token)) {
+            var email = jwtProvider.getEmailFromToken(token);
+            var userDetails = userDetailsService.loadUserByUsername(email);
+            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return Pattern.matches(API_WHITE_LIST, request.getRequestURI());
+    }
+}
