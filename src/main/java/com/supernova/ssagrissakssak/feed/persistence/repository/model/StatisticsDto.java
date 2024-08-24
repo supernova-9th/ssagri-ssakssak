e40ff17a0ec902990c6a92224f9e2e768ea0576d
harok <dorianharok@github.com>
2024-08-24 23:55:53 +0900

    refactor: StatisticsDto 생성자 변경

--- src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/model/StatisticsDto.java
+++ src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/model/StatisticsDto.java
@@ -4,10 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @AllArgsConstructor
 public class StatisticsDto {
     private LocalDateTime dateTime;
     private Long value;
+
+    public StatisticsDto(String dateTimeStr, Long value) {
+        this.dateTime = parseDateTime(dateTimeStr);
+        this.value = value;
+    }
+
+    private LocalDateTime parseDateTime(String dateTimeStr) {
+        if (dateTimeStr.length() > 10) {
+            return LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        return LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
 }
