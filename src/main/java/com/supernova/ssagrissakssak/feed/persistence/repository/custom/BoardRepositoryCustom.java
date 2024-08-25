374bc01834618af6fa9591056b6a0eac4ba74d8c
harok <dorianharok@github.com>
2024-08-25 21:31:47 +0900

    refactor: getStatistics 파라미터 및 쿼리 수정

--- src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/custom/BoardRepositoryCustom.java
+++ src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/custom/BoardRepositoryCustom.java
@@ -1,5 +1,6 @@
 package com.supernova.ssagrissakssak.feed.persistence.repository.custom;
 
+import com.supernova.ssagrissakssak.core.enums.StatisticsTimeType;
 import com.supernova.ssagrissakssak.core.enums.StatisticsType;
 import com.supernova.ssagrissakssak.feed.persistence.repository.model.StatisticsDto;
 
@@ -7,5 +8,5 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface BoardRepositoryCustom {
-    List<StatisticsDto> getStatistics(String hashtag, StatisticsType type, LocalDate start, LocalDate end);
+    List<StatisticsDto> getStatistics(String hashtag, StatisticsType type, StatisticsTimeType timeType, LocalDate start, LocalDate end);
 }
--- src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/impl/BoardRepositoryImpl.java
+++ src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/impl/BoardRepositoryImpl.java
@@ -1,17 +1,23 @@
 package com.supernova.ssagrissakssak.feed.persistence.repository.impl;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.DateTimePath;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.supernova.ssagrissakssak.core.enums.StatisticsTimeType;
 import com.supernova.ssagrissakssak.core.enums.StatisticsType;
 import com.supernova.ssagrissakssak.feed.persistence.repository.custom.BoardRepositoryCustom;
 import com.supernova.ssagrissakssak.feed.persistence.repository.model.StatisticsDto;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.supernova.ssagrissakssak.feed.persistence.repository.entity.QBoardEntity.boardEntity;
 import static com.supernova.ssagrissakssak.feed.persistence.repository.entity.QHashtagEntity.hashtagEntity;
+
+import com.querydsl.core.types.dsl.StringTemplate;
 
 @RequiredArgsConstructor
 public class BoardRepositoryImpl implements BoardRepositoryCustom {
@@ -19,20 +25,38 @@ public class BoardRepositoryImpl implements BoardRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<StatisticsDto> getStatistics(String hashtag, StatisticsType type, LocalDate start, LocalDate end) {
+    public List<StatisticsDto> getStatistics(String hashtag, StatisticsType type, StatisticsTimeType timeType, LocalDate start, LocalDate end) {
         return jpaQueryFactory
                 .select(Projections.constructor(StatisticsDto.class,
-                        boardEntity.createdAt,
+                        getDateTimeExpression(boardEntity.createdAt, timeType),
                         type.getValueExpression()
                 ))
                 .from(boardEntity)
                 .join(hashtagEntity).on(boardEntity.id.eq(hashtagEntity.boardId))
                 .where(
                         hashtagEntity.hashtag.eq(hashtag),
-                        boardEntity.createdAt.between(start.atStartOfDay(), end.atStartOfDay())
+                        boardEntity.createdAt.between(start.atStartOfDay(), end.plusDays(1).atStartOfDay())
                 )
-                .groupBy(boardEntity.createdAt)
-                .orderBy(boardEntity.createdAt.asc())
+                .groupBy(getDateTimeExpression(boardEntity.createdAt, timeType))
+                .orderBy(getDateTimeExpression(boardEntity.createdAt, timeType).asc())
                 .fetch();
+    }
+
+    private StringTemplate getDateTimeExpression(DateTimePath<LocalDateTime> createdAt, StatisticsTimeType timeType) {
+        if (timeType == StatisticsTimeType.DATE) {
+            return Expressions.stringTemplate(
+                    "DATE_FORMAT({0}, {1})",
+                    createdAt,
+                    "%Y-%m-%d"
+            );
+        } else if (timeType == StatisticsTimeType.HOUR) {
+            return Expressions.stringTemplate(
+                    "DATE_FORMAT({0}, {1})",
+                    createdAt,
+                    "%Y-%m-%d %H:00:00"
+            );
+        } else {
+            throw new IllegalArgumentException("Invalid time type");
+        }
     }
 }
