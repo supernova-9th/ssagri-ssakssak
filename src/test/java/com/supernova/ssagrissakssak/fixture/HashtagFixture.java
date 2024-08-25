52c5ec3d763f4433f319e5fb26e742a951162d0b
harok <dorianharok@github.com>
2024-08-25 21:32:24 +0900

    test: test를 위한 fixture 정의 #21

---
+++ src/test/java/com/supernova/ssagrissakssak/fixture/HashtagFixture.java
@@ -0,0 +1,21 @@
+package com.supernova.ssagrissakssak.fixture;
+
+import com.supernova.ssagrissakssak.feed.persistence.repository.entity.HashtagEntity;
+
+public class HashtagFixture {
+
+    public static HashtagEntity get(Long boardId) {
+        return HashtagEntity.builder()
+                .hashtag("test")
+                .boardId(boardId)
+                .build();
+    }
+
+    public static HashtagEntity get(String hashtag, Long boardId) {
+        return HashtagEntity.builder()
+                .hashtag(hashtag)
+                .boardId(boardId)
+                .build();
+    }
+
+}
