5805abda573c4f6152febc50e1687adbcca2628c
hyeji <hyeji6742@naver.com>
2024-08-26 21:51:36 +0900

    test: 게시물 목록 테스트 #6

--- src/test/java/com/supernova/ssagrissakssak/feed/service/BoardServiceTest.java
+++ src/test/java/com/supernova/ssagrissakssak/feed/service/BoardServiceTest.java
@@ -1,11 +1,12 @@
 package com.supernova.ssagrissakssak.feed.service;
 
-import com.supernova.ssagrissakssak.core.enums.ContentType;
 import com.supernova.ssagrissakssak.feed.controller.response.BoardDetailResponse;
+import com.supernova.ssagrissakssak.feed.controller.response.BoardResponse;
 import com.supernova.ssagrissakssak.feed.persistence.repository.BoardRepository;
 import com.supernova.ssagrissakssak.feed.persistence.repository.HashtagRepository;
 import com.supernova.ssagrissakssak.feed.persistence.repository.entity.BoardEntity;
-import com.supernova.ssagrissakssak.feed.persistence.repository.entity.HashtagEntity;
+import com.supernova.ssagrissakssak.fixture.BoardFixture;
+import com.supernova.ssagrissakssak.fixture.HashtagFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,34 +36,8 @@ class BoardServiceTest {
 
     @BeforeEach
     void setUp() {
-        boardRepository.deleteAll();
-        hashtagRepository.deleteAll();
-
-        BoardEntity board = BoardEntity.builder()
-                .id(1L)
-                .type(ContentType.FACEBOOK)
-                .title("제목 테스트1")
-                .content("내용 테스트1")
-                .viewCount(10)
-                .likeCount(5)
-                .shareCount(2)
-                .build();
-        savedBoard1 = boardRepository.save(board);
-
-        HashtagEntity hashtag1 = HashtagEntity.builder()
-                .id(1L)
-                .hashtag("#맛집")
-                .boardId(savedBoard1.getId())
-                .build();
-        hashtagRepository.save(hashtag1);
-
-        HashtagEntity hashtag2 = HashtagEntity.builder()
-                .id(2L)
-                .hashtag("#여행")
-                .boardId(savedBoard1.getId())
-                .build();
-        hashtagRepository.save(hashtag2);
-
+        boardRepository.deleteAllInBatch();
+        hashtagRepository.deleteAllInBatch();
     }
 
     @Test
@@ -95,7 +70,87 @@ class BoardServiceTest {
         BoardDetailResponse result = boardService.getBoard(savedBoard1.getId());
 
         // Then
-        assertEquals(result.getViewCount(), viewCount+1);
+        assertEquals(result.getViewCount(), viewCount + 1);
     }
 
+    @Test
+    void 모든파라미터_입력해서_게시물_목록_검색한다() {
+
+        // Given
+        BoardEntity board1 = boardRepository.save(BoardFixture.get(10, 2, 3));
+        BoardEntity board2 = boardRepository.save(BoardFixture.get(20, 2, 3));
+
+        hashtagRepository.save(HashtagFixture.get(board1.getId()));
+        hashtagRepository.save(HashtagFixture.get(board2.getId()));
+
+        // Given
+        String hashtag = "test";
+        String type = "FACEBOOK";
+        String orderBy = "created_at";
+        String searchBy = "title";
+        String search = "Test";
+        Integer pageCount = 10;
+        Integer page = 0;
+
+        // When
+        List<BoardResponse> result = boardService.getBoards(hashtag, type, orderBy, searchBy, search, pageCount, page);
+
+        // Then
+        assertEquals(2, result.size());
+        assertEquals(board1.getContent(), result.get(0).getContent());
+        assertEquals(board2.getContent(), result.get(1).getContent());
+        assertEquals(board1.getTitle(), result.get(0).getTitle());
+        assertEquals(board2.getTitle(), result.get(1).getTitle());
+    }
+
+    @Test
+    void 해시태그_완전일치_하지않으면_게시물목록_검색되지_않는다() {
+
+        // Given
+        BoardEntity board1 = boardRepository.save(BoardFixture.get(10, 2, 3));
+        BoardEntity board2 = boardRepository.save(BoardFixture.get(20, 2, 3));
+
+        hashtagRepository.save(HashtagFixture.get(board1.getId()));
+        hashtagRepository.save(HashtagFixture.get(board2.getId()));
+
+        // Given
+        String hashtag = "tes";
+        String type =null;
+        String orderBy = null;
+        String searchBy = null;
+        String search = null;
+        Integer pageCount = null;
+        Integer page = null;
+
+        // When
+        List<BoardResponse> result = boardService.getBoards(hashtag, type, orderBy, searchBy, search, pageCount, page);
+
+        // Then
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void 게시물목록_조회시_내용_글자수_20_제한() {
+
+        // Given
+        BoardEntity board1 = boardRepository.save(BoardFixture.get("제목","내용 테스트 20자 이상 문자열입니다.",10, 2, 3));
+
+        hashtagRepository.save(HashtagFixture.get(board1.getId()));
+
+        // Given
+        String hashtag = null;
+        String type =null;
+        String orderBy = null;
+        String searchBy = null;
+        String search = null;
+        Integer pageCount = null;
+        Integer page = null;
+
+        // When
+        List<BoardResponse> result = boardService.getBoards(hashtag, type, orderBy, searchBy, search, pageCount, page);
+
+        // Then
+        assertNotEquals(board1.getContent().length(), result.get(0).getContent().length());
+        assertTrue(result.get(0).getContent().length() <= 20);
+    }
 }
---
+++ src/test/java/com/supernova/ssagrissakssak/fixture/BoardFixture.java
@@ -0,0 +1,51 @@
+package com.supernova.ssagrissakssak.fixture;
+
+import com.supernova.ssagrissakssak.core.enums.ContentType;
+import com.supernova.ssagrissakssak.feed.persistence.repository.entity.BoardEntity;
+
+public class BoardFixture {
+
+    public static BoardEntity get() {
+        return BoardEntity.builder()
+                .type(ContentType.FACEBOOK)
+                .title("Test")
+                .content("Content")
+                .viewCount(10)
+                .likeCount(5)
+                .shareCount(2)
+                .build();
+    }
+
+    public static BoardEntity get(int viewCount, int likeCount, int shareCount) {
+        return BoardEntity.builder()
+                .type(ContentType.FACEBOOK)
+                .title("Test")
+                .content("Content")
+                .viewCount(viewCount)
+                .likeCount(likeCount)
+                .shareCount(shareCount)
+                .build();
+    }
+
+    public static BoardEntity get(ContentType contentType, int viewCount, int likeCount, int shareCount) {
+        return BoardEntity.builder()
+                .type(contentType)
+                .title("Test")
+                .content("Content")
+                .viewCount(viewCount)
+                .likeCount(likeCount)
+                .shareCount(shareCount)
+                .build();
+    }
+
+    public static BoardEntity get(String title, String content, int viewCount, int likeCount, int shareCount) {
+        return BoardEntity.builder()
+                .type(ContentType.FACEBOOK)
+                .title(title)
+                .content(content)
+                .viewCount(viewCount)
+                .likeCount(likeCount)
+                .shareCount(shareCount)
+                .build();
+    }
+}
