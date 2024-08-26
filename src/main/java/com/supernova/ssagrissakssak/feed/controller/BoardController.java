4bf2b1213b8046a5bcbde293f1a1811e43747c21
nyximos <nyximos@gmail.com>
2024-08-26 19:31:53 +0900

    refactor: @AuthenticationPrincipal 적용 #27

--- src/main/java/com/supernova/ssagrissakssak/feed/controller/BoardController.java
+++ src/main/java/com/supernova/ssagrissakssak/feed/controller/BoardController.java
@@ -3,11 +3,13 @@ package com.supernova.ssagrissakssak.feed.controller;
 import com.supernova.ssagrissakssak.core.enums.StatType;
 import com.supernova.ssagrissakssak.core.enums.StatValueType;
 import com.supernova.ssagrissakssak.core.security.JwtProvider;
+import com.supernova.ssagrissakssak.core.security.LoginUser;
 import com.supernova.ssagrissakssak.core.wrapper.ResultResponse;
 import com.supernova.ssagrissakssak.feed.controller.response.StatResponse;
 import com.supernova.ssagrissakssak.feed.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -20,21 +22,20 @@ import static com.supernova.ssagrissakssak.core.constants.CommonConstant.HEADER_
 @RequestMapping("/boards")
 public class BoardController {
 
-    private final JwtProvider jwtProvider;
     private final BoardService boardService;
 
     @GetMapping("/stats")
     public ResultResponse<List<StatResponse>> getStats(
-            @RequestHeader(value = HEADER_AUTHORIZE_TOKEN) String accessToken,
+            @AuthenticationPrincipal LoginUser user,
             @RequestParam(required = false) String hashtag,
             @RequestParam StatType statType,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end,
             @RequestParam(required = false, defaultValue = "count") StatValueType statValueType
     ) {
-        if (hashtag == null) { hashtag = jwtProvider.getEmailFromToken(accessToken);}
-        if (start == null) {start = LocalDate.now().minusDays(7);}
-        if (end == null) {end = LocalDate.now();}
+        if (hashtag == null) { hashtag = user.getUsername(); }
+        if (start == null) { start = LocalDate.now().minusDays(7); }
+        if (end == null) { end = LocalDate.now(); }
         return new ResultResponse<>(boardService.getStats(hashtag, statType, start, end, statValueType));
     }
 }
