a2b284c98f9ff6238a2a70a7ba6de9c712b4a8d5
hyeji <hyeji6742@naver.com>
2024-08-26 12:02:00 +0900

    feat: 게시물 상세 조회 #7

---
+++ src/main/java/com/supernova/ssagrissakssak/feed/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.supernova.ssagrissakssak.feed.controller;
+
+import com.supernova.ssagrissakssak.core.wrapper.ResultResponse;
+import com.supernova.ssagrissakssak.feed.controller.response.BoardDetailResponse;
+import com.supernova.ssagrissakssak.feed.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boards")
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    /* 게시물 상세 조회 */
+    @GetMapping("/{id}")
+    public ResultResponse<BoardDetailResponse> getBoard(@PathVariable Long id) {
+        return new ResultResponse<>(boardService.getBoard(id));
+    }
+}
--- src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/entity/BoardEntity.java
+++ src/main/java/com/supernova/ssagrissakssak/feed/persistence/repository/entity/BoardEntity.java
@@ -35,4 +35,5 @@ public class BoardEntity extends BaseEntity {
 
     @Column(name = "share_count", nullable = false)
     private int shareCount;
+
 }
