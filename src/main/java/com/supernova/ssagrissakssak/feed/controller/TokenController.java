eaf835c72a045c496cfb7753ff5ade8a64a87a03
nyximos <nyximos@gmail.com>
2024-08-28 12:58:43 +0900

    docs: Controller 주석 추가 #31

--- src/main/java/com/supernova/ssagrissakssak/feed/controller/TokenController.java
+++ src/main/java/com/supernova/ssagrissakssak/feed/controller/TokenController.java
@@ -1,5 +1,7 @@
 package com.supernova.ssagrissakssak.feed.controller;
 
+import com.supernova.ssagrissakssak.core.exception.JwtValidateException;
+import com.supernova.ssagrissakssak.core.exception.UserNotFoundException;
 import com.supernova.ssagrissakssak.core.wrapper.ResultResponse;
 import com.supernova.ssagrissakssak.feed.controller.response.TokenResponse;
 import com.supernova.ssagrissakssak.feed.service.TokenService;
@@ -16,6 +18,15 @@ public class TokenController {
 
     private final TokenService tokenService;
 
+    /**
+     * 토큰을 재발급합니다.
+     *
+     * @param refreshToken 리프레시 토큰.
+     *        HTTP 헤더에 포함되어 전달됩니다.
+     * @return 새로운 액세스 토큰과 리프레시 토큰을 포함하는 객체.
+     * @throws JwtValidateException 토큰이 만료되었거나 유효하지 않은 경우
+     * @throws UserNotFoundException 지정된 email의 사용자를 찾을 수 없는 경우
+    */
     @PostMapping("/auth/token/reissue")
     public ResultResponse<TokenResponse> reissue(@RequestHeader(HEADER_AUTHORIZE_TOKEN) String refreshToken) {
         return new ResultResponse<>(tokenService.reissue(refreshToken));
--- src/main/java/com/supernova/ssagrissakssak/feed/controller/UserController.java
+++ src/main/java/com/supernova/ssagrissakssak/feed/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.supernova.ssagrissakssak.feed.controller;
 
+import com.supernova.ssagrissakssak.core.exception.InvalidPasswordException;
+import com.supernova.ssagrissakssak.core.exception.UserNotFoundException;
 import com.supernova.ssagrissakssak.core.wrapper.ResultResponse;
 import com.supernova.ssagrissakssak.feed.controller.request.ApproveRequest;
 import com.supernova.ssagrissakssak.feed.controller.request.SignInRequest;
@@ -7,6 +9,8 @@ import com.supernova.ssagrissakssak.feed.controller.request.UserCreateRequest;
 import com.supernova.ssagrissakssak.feed.controller.response.DefaultIdResponse;
 import com.supernova.ssagrissakssak.feed.controller.response.TokenResponse;
 import com.supernova.ssagrissakssak.feed.service.UserService;
+import com.supernova.ssagrissakssak.feed.service.delegator.validator.ActiveStatusValidator;
+import com.supernova.ssagrissakssak.feed.service.delegator.validator.VerificationCodeValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,12 +31,37 @@ public class UserController {
         return new ResultResponse<>(DefaultIdResponse.of(joined));
     }
 
+    /**
+     * 사용자 승인 요청을 처리합니다.
+     *
+     * @param approveRequest 승인 요청 정보를 담고 있는 객체.
+     *        이 객체는 다음 필드를 포함해야 합니다:
+     *        - email: 사용자의 이메일 주소 (필수)
+     *        - password: 사용자의 비밀번호 (필수)
+     *        - authenticationCode: 사용자의 인증 코드 (필수, 6자리, 숫자와 영문자로만 구성)
+     * @return 처리 결과를 나타내는 응답 객체.
+     * @throws UserNotFoundException 지정된 email의 사용자를 찾을 수 없는 경우
+     * @throws InvalidPasswordException 잘못된 비밀번호인 경우
+     * @throws VerificationCodeValidator 인증 코드가 올바르지 않은 경우
+     * @throws ActiveStatusValidator 이미 활성화 상태인 계정인 경우
+     */
     @PutMapping("/auth/users/approve")
     public ResultResponse<Void> approve(@Valid @RequestBody ApproveRequest approveRequest) {
         userService.approve(approveRequest);
         return new ResultResponse<>();
     }
 
+    /**
+     * 로그인 요청을 처리합니다.
+     *
+     * @param signInRequest 로그인 요청 정보를 담고 있는 객체.
+     *        이 객체는 다음 필드를 포함해야 합니다:
+     *        - email: 이메일 주소 (필수)
+     *        - password: 비밀번호 (필수)
+     * @return 액세스 토큰과 리프레시 토큰을 포함하는 응답 객체.
+     * @throws UserNotFoundException 활성화된 계정이 존재하지 않을 경우
+     * @throws InvalidPasswordException 잘못된 비밀번호인 경우
+     */
     @PostMapping("/auth/users/sign-in")
     public ResultResponse<TokenResponse> signIn(@Valid @RequestBody SignInRequest signInRequest) {
         return new ResultResponse<>(userService.signIn(signInRequest));
